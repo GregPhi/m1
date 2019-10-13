f8712933725c8a84beb00224be53f276da45524c
GregPhi <gregoire.philippot@laposte.net>
2019-10-13 21:13:08 +0200

    update et info en un fichier -> info

--- pje-android/ApplicContact/app/src/main/java/com/example/appliccontact/InfoContactActivity.java
+++ pje-android/ApplicContact/app/src/main/java/com/example/appliccontact/InfoContactActivity.java
@@ -3,36 +3,46 @@ package com.example.appliccontact;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InfoContactActivity extends AppCompatActivity {
 
+    private EditText nomEdit;
+    private EditText ageEdit;
+
     private Contact current;
-    private Bundle bundle;
+    private Bundle currentBundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.infos_contact);
+
         Intent in = getIntent();
-        bundle = in.getExtras();
-        int id = bundle.getInt("id");
-        TextView nom = findViewById(R.id.nom);
-        String no = bundle.getString("nom");
-        nom.setText(no);
-        TextView age = findViewById(R.id.age);
-        String ag = bundle.getString("age");
-        age.setText(ag);
-        current = new Contact(no,ag);
+        currentBundle = in.getExtras();
+        int id = currentBundle.getInt("id");
+        String nom = currentBundle.getString("nom");
+        String age = currentBundle.getString("age");
+        current = new Contact(nom,age);
         current.setId(id);
+
+        nomEdit = findViewById(R.id.info_nom);
+        nomEdit.setText(getString(R.string.info_nom,nom));
+
+        ageEdit = findViewById(R.id.info_age);
+        ageEdit.setText(getString(R.string.info_age,age));
     }
 
     public void updateContact(View view){
-        Intent in = new Intent(this,UpdateContactActivity.class);
-        in.putExtras(bundle);
-        startActivity(in);
+        String nom = nomEdit.getText().toString();
+        String age = ageEdit.getText().toString();
+        current.setNom(nom);
+        current.setAge(age);
+        MainActivity.mContactViewModel.updateContact(current);
+        retourMain(view);
     }
 
     public void retourMain(View view){
