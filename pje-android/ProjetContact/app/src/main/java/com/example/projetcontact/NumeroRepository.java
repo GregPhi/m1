3bd53a0abf5d69ebe8210ee446b55ba155c4ea79
GregPhi <gregoire.philippot@laposte.net>
2019-10-17 15:42:54 +0200

    soucis getallNumForId résolu && pbl insert numero code 787

--- pje-android/ProjetContact/app/src/main/java/com/example/projetcontact/NumeroRepository.java
+++ pje-android/ProjetContact/app/src/main/java/com/example/projetcontact/NumeroRepository.java
@@ -2,13 +2,8 @@ package com.example.projetcontact;
 
 import android.app.Application;
 import android.os.AsyncTask;
-
 import java.util.List;
-import java.util.ArrayList;
-import java.util.NavigableMap;
-
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 public class NumeroRepository {
     public static LiveData<List<Numero>> mNumerosForId;
@@ -26,20 +21,9 @@ public class NumeroRepository {
     public void insert (Numero numero){ new insertAsyncTask(mNumeroDao).execute(numero);}
 
     LiveData<List<Numero>> getAllNumeroForAContact(Contact contact) {
-        new getNumeroForId(mNumeroDao).execute(contact);
-        return mNumerosForId;
+        mNumerosForId = mNumeroDao.getAllNumeroForAContact(contact.getId());
+        return  mNumerosForId;
     }
-
-    /*ArrayList<Numero> res = new ArrayList<Numero>();
-        List<Numero> ns = mAllNumeros.getValue();
-        for(Numero n : ns){
-            if(n.getContactId() == id){
-                res.add(n);
-            }
-        }
-        MutableLiveData<List<Numero>> r = new MutableLiveData<List<Numero>>();
-        r.setValue(res);
-        return r;*/
 
     private static class insertAsyncTask extends AsyncTask<Numero, Void, Void> {
 
@@ -52,21 +36,6 @@ public class NumeroRepository {
         @Override
         protected Void doInBackground(final Numero... params) {
             mAsyncTaskDao.insert(params[0]);
-            return null;
-        }
-    }
-
-    private static class getNumeroForId extends AsyncTask<Contact, Void, Void> {
-
-        private NumeroDao mAsyncTaskDao;
-
-        getNumeroForId(NumeroDao dao) {
-            mAsyncTaskDao = dao;
-        }
-
-        @Override
-        protected Void doInBackground(final Contact... params) {
-            NumeroRepository.mNumerosForId = mAsyncTaskDao.getAllNumeroForAContact(params[0].getId());
             return null;
         }
     }
